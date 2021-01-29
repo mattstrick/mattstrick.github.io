b4dc1bd0ff4670f3d9719cfeffe0233b93415737
Matt Strick <matt.strick@gmail.com>
2021-01-28 18:46:48 -0600

    v1.2.1 make details page use API and DB

--- maps/api/objects/gym.php
+++ maps/api/objects/gym.php
@@ -35,5 +35,39 @@ class Gym{
     
         return $stmt;
     }
+
+    // used when filling up the update product form
+    function readOne(){
+    
+        // query to read single record
+        $query = "SELECT
+                    name, lat, lng, address, phone
+                FROM
+                    " . $this->table_name . " p                    
+                WHERE
+                    p.lat = ? AND p.lng = ?
+                LIMIT
+                    0,1";
+    
+        // prepare query statement
+        $stmt = $this->conn->prepare( $query );
+    
+        // bind id of product to be updated
+        $stmt->bindParam(1, $this->lat);
+        $stmt->bindParam(2, $this->lng);
+    
+        // execute query
+        $stmt->execute();
+    
+        // get retrieved row
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+        // set values to object properties
+        $this->name = $row['name'];
+        $this->lat = $row['lat'];
+        $this->lng = $row['lng'];
+        $this->address = $row['address'];
+        $this->phone = $row['phone'];
+    }
 }
 ?>
