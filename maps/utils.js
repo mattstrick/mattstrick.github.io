77e6943304a383651bcbc02030b04503ef453b61
Matt Strick <matt.strick@gmail.com>
2021-01-30 21:34:28 -0600

    v1.2.5 pretty print price on details and search

---
+++ maps/utils.js
@@ -0,0 +1,25 @@
+function getPrettyPrice(price) {
+    switch (price) {
+        case 0:
+            return 'None Given';
+            break;
+        case 1:
+            return 'Less than $30/month';
+            break;
+        case 2:
+            return 'Less than $60/month';
+            break;
+        case 3:
+            return 'Less than $90/month';
+            break;
+        case 4:
+            return 'Less than $120/month';
+            break;
+        case 5:
+            return 'More than $120/month';
+            break;
+        default:
+            return 'None Given';
+            break;
+    }
+}
